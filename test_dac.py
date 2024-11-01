1189284e0ce9dbf6b28baf48d1d0f4f3973c4db5
Peter Traver <pdt9929@cm020.hpc.nyu.edu>
2024-11-01 12:42:17 -0400

    Cleaned dac script

--- test_dac.py
+++ test_dac.py
@@ -4,33 +4,6 @@ from audiotools import AudioSignal
 #import d_models
 import torch
 
-# #create style encoders
-# timbre_enc = d_models.style_enc(128)
-# pitch_enc = d_models.style_enc(1)
-# loudness_enc = d_models.style_enc(1)
-
-# #create content enc
-# content_enc = d_models.content_enc()
-
-# #create decoder
-# decoder = d_models.decoder(1154)
-
-# #make test embedding
-# test_e = torch.zeros(5, 1024,2411)
-
-# #get encoder outputs
-# out_ts = timbre_enc(test_e)
-# out_ps  = pitch_enc(test_e)
-# out_ls = loudness_enc(test_e)
-
-# out_c = content_enc(test_e)[0]
-
-# #decode
-# print(decoder(torch.cat((out_ts, out_ps, out_ls, out_c),1)).shape)
-
-
-# exit()
-
 # Download a model
 model_path = dac.utils.download(model_type="44khz")
 model = dac.DAC.load(model_path)
@@ -61,7 +34,6 @@ z_fromlatents = model.quantizer.from_latents(latents)[0]
 # AudioSignal(rec.detach(),44100).write("z.wav")
 # AudioSignal(rec_fromcodes.detach(),44100).write("z_fromcodes.wav")
 # AudioSignal(rec_fromlatents.detach(),44100).write("z_fromlatents.wav")
-
 
 # print(rec.shape)
 # print(z_fromcodes == z)
